39381ea4ab1410560674f2d4848fc61e0c750eb8
Geechang Lee <geechanglee@Geechangui-MacBookPro.local>
2022-02-15 07:53:26 +0900

    page 추가

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+
+const ChatDetail : NextPage = () => {
+    return(
+        <div className="py-10 px-4">
+            <div>
+                <div/>
+                <div>
+                    Hi how much are you selling the iphone
+                </div>
+            </div>
+            <div>
+                <div/>
+                <div>
+                    I want 20000won
+                </div>
+            </div>
+            <div>
+                <div/>
+                <div>
+                    미쳤어
+                </div>
+            </div>
+            <div>
+                <div>
+                    <input type='text'/>
+                    <div>
+                        <span>&arr;</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatDetail
