90ec7aee03144938b09e0f579a7ce9f29dd709a9
monostylegc <monostylegc@gmail.com>
2022-02-14 17:32:51 +0900

    pages upload community

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -20,18 +20,18 @@ export default function Enter () {
                     </div>
                 </div>
                 <form className="flex flex-col mt-8">
-                    <label className="text-sm font-medium text-gray-700">
+                    <label htmlFor="input" className="text-sm font-medium text-gray-700">
                         {method === "email" ? "Email address" : null}
                         {method === "phone" ? "Phone number" : null}
                     </label>
                     <div className="mt-2">
-                        {method === "email" ? <input type="email" required className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500" /> : null}
+                        {method === "email" ? <input id="input" type="email" required className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500" /> : null}
                         {method === "phone" ? (
                             <div className="flex rounded-md shadow-sm ">
                                 <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
                                     +82
                                 </span>
-                                <input type="number" required className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500" />
+                                <input id="input" type="number" required className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:ring-orange-500 focus:border-orange-500" />
                             </div>
                         ) : null}
                     </div>
