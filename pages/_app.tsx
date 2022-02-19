cace46c4bcd5af79ab26d711730f2df867fa42da
monostylegc <monostylegc@gmail.com>
2022-02-19 12:33:04 +0900

    nextauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp ({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
-    <div className='w-full max-w-xl mx-auto'>
-      <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div className='w-full max-w-xl mx-auto'>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
   )
 }
 
