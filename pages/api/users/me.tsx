f6e6703fed724ae80b6ba317b9232986b960bd7c
Geechang Lee <geechanglee@Geechangui-MacBookPro.local>
2022-02-23 08:05:42 +0900

    다시 commit

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -3,8 +3,7 @@ import client from "@libs/client";
 import withHandler, {ResponseType} from "@libs/server/withhandler";
 
 function handler (req: NextApiRequest,res: NextApiResponse<ResponseType>) {
-    console.log(req.body);
-    return res.json({ ok: true, message : 'GooD~' })
+    
 }
 
 export default withHandler({methods:['GET'], handler})
