a058ed499eb1f782742d678eaf56169119986049
Geechang Lee <geechanglee@Geechangui-MacBookPro.local>
2022-02-23 08:05:12 +0900

    bug fix

--- libs/server/withhandler.ts
+++ libs/server/withhandler.ts
@@ -4,30 +4,32 @@ import { getSession } from "next-auth/react";
 export interface ResponseType {
     ok: boolean;
     [key: string]: any;
-}
+  }
+
+type method = "GET" | "POST" | "DELETE"
 
 interface ConfigType {
-    method: "GET" | "POST" | "DELETE",
-    handler: (req: NextApiRequest,
-        res: NextApiResponse) => void,
+    methods: method[];
+    handler: (req: NextApiRequest,res: NextApiResponse) => void,
     isPrivate?: boolean
 }
 
-export default async function withHandler (
-    { method,
-        isPrivate = true,
-        handler }: ConfigType) {
-    return async function (req: NextApiRequest, res: NextApiResponse): Promise<any> {
+export default function withHandler 
+({ methods,isPrivate = true,handler }: ConfigType) 
+{
+    return async function 
+    (req: NextApiRequest, res: NextApiResponse): Promise<any> 
+    {
         const session = await getSession({ req })
 
-        if (req.method !== method) {
+        if (req.method && !methods.includes(req.method as any)) {
             return res.status(405).end();
         }
         if (isPrivate && !session) {
             return res.status(401).json({ ok: false, error: "Login first, plz." });
         }
         try {
-            await handler(req, res)
+            handler(req, res)
         } catch (error) {
             console.log(error);
             return res.status(500).json({ error });
--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -1,18 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/client";
-import withHandler from "@libs/server/withhandler";
+import withHandler, {ResponseType} from "@libs/server/withhandler";
 
-async function handler (
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
+function handler (req: NextApiRequest,res: NextApiResponse<ResponseType>) {
     console.log(req.body);
-    res.json({ ok: true, message: 'GooD~' })
+    return res.json({ ok: true, message : 'GooD~' })
 }
 
-export default () => {
-    withHandler({
-        method: "GET",
-        handler
-    })
-};
+export default withHandler({methods:['GET'], handler})
