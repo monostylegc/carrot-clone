c1c3e39bfc309780ddeab0b03b75639b97f5f76a
monostylegc <monostylegc@gmail.com>
2022-03-03 18:06:47 +0900

    a

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -5,14 +5,22 @@ import TextArea from "@components/textarea";
 import { useForm } from 'react-hook-form';
 import useMutation from '@libs/client/usemutation';
 
+interface uploadPostForm {
+    question: string;
+}
+
 const Write: NextPage = () => {
-    const { register, handleSubmit } = useForm()
-    const [] = useMutation('')
+    const { register, handleSubmit } = useForm<uploadPostForm>()
+    const [uploadPost] = useMutation('/api/community/')
+
+    const onValid = (data: uploadPostForm) => {
+        uploadPost(data)
+    }
 
     return (
         <Layout canGoBack title="Write Post">
-            <form className="p-4 space-y-4">
-                <TextArea register={register('post', { required: true })} required placeholder="Ask a question!" />
+            <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
+                <TextArea register={register('question', { required: true })} required placeholder="Ask a question!" />
                 <Button text="Submit" />
             </form>
         </Layout>
